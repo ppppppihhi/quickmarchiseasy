0f7e73191f5daa2acb4f0aed114809c33d91a8f7
ppppppihhi <cobexuquang.abc@gmail.com>
2026-08-28 08:57:55 +0700

    test04.js

---
+++ tests/writing/test04.js
@@ -0,0 +1,302 @@
+const TEST_DATA = {
+
+    title: "Writing Test 04",
+
+    /* =====================================================
+       TASK 3
+    ===================================================== */
+
+    task3: [
+
+        {
+            answer:
+                "Which clubs are you going to, Ben?",
+
+            acceptedQuestions: [
+                "Which clubs are you going to, Ben?",
+                "What clubs are you going to, Ben?",
+                "Which clubs are you going to?"
+            ],
+
+            explanation:
+                "Which clubs...? = hỏi về những câu lạc bộ nào.<br>" +
+                "are you going to = bạn sẽ tham gia."
+        },
+
+        {
+            answer:
+                "Which do you like better, computers or football?",
+
+            acceptedQuestions: [
+                "Which do you like better, computers or football?",
+                "Do you like computers or football better?"
+            ],
+
+            explanation:
+                "Which ... better? = hỏi thích cái nào hơn.<br>" +
+                "computers or football = máy tính hay bóng đá."
+        },
+
+        {
+            answer:
+                "Was there a computer club last year?",
+
+            acceptedQuestions: [
+                "Was there a computer club last year?",
+                "Did you have a computer club last year?"
+            ],
+
+            explanation:
+                "Was there...? = hỏi có ... hay không.<br>" +
+                "last year = năm ngoái."
+        },
+
+        {
+            answer:
+                "How many children are going to football club?",
+
+            acceptedQuestions: [
+                "How many children are going to football club?",
+                "How many children are going to the football club?"
+            ],
+
+            explanation:
+                "How many...? = hỏi có bao nhiêu.<br>" +
+                "children = trẻ em / học sinh."
+        },
+
+        {
+            answer:
+                "Can I come to football club?",
+
+            acceptedQuestions: [
+                "Can I come to football club?",
+                "Can I come to the football club?",
+                "Can I go to football club?",
+                "Can I go to the football club?"
+            ],
+
+            explanation:
+                "Can I...? = hỏi xin phép / hỏi mình có thể làm gì không.<br>" +
+                "come to football club = đến câu lạc bộ bóng đá."
+        }
+
+    ],
+
+
+    /* =====================================================
+       TASK 5
+    ===================================================== */
+
+    task5: [
+
+        {
+            sentence:
+                "Last week the school clubs (example) ________. I went to computer club and football club.",
+
+            verb: "start",
+
+            answer: "started",
+
+            explanation:
+                "start → V2/ed: started.<br>" +
+                "started = đã bắt đầu."
+        },
+
+        {
+            sentence:
+                "Mr Lee (1) ________ to us about different computer programmes.",
+
+            verb: "talk",
+
+            answer: "talked",
+
+            explanation:
+                "talk → V2/ed: talked.<br>" +
+                "talked to us = đã nói chuyện với chúng tôi."
+        },
+
+        {
+            sentence:
+                "Then we (2) ________ partners.",
+
+            verb: "choose",
+
+            answer: "chose",
+
+            explanation:
+                "choose → V2/ed: chose.<br>" +
+                "chose partners = đã chọn bạn cùng nhóm."
+        },
+
+        {
+            sentence:
+                "and (3) ________ at some great websites.",
+
+            verb: "look",
+
+            answer: "looked",
+
+            explanation:
+                "look → V2/ed: looked.<br>" +
+                "looked at = đã xem / nhìn vào."
+        },
+
+        {
+            sentence:
+                "After that Mr Lee (4) ________ us play computer games for five minutes!",
+
+            verb: "let",
+
+            answer: "let",
+
+            explanation:
+                "let → V2/ed: let.<br>" +
+                "let us play = cho chúng tôi chơi."
+        },
+
+        {
+            sentence:
+                "We all really (5) ________ football club.",
+
+            verb: "enjoy",
+
+            answer: "enjoyed",
+
+            explanation:
+                "enjoy → V2/ed: enjoyed.<br>" +
+                "enjoyed football club = đã rất thích câu lạc bộ bóng đá."
+        },
+
+        {
+            sentence:
+                "First we (6) ________ up and down and warmed up.",
+
+            verb: "jump",
+
+            answer: "jumped",
+
+            explanation:
+                "jump → V2/ed: jumped.<br>" +
+                "jumped up and down = đã nhảy lên nhảy xuống."
+        },
+
+        {
+            sentence:
+                "Everybody (7) ________ really well.",
+
+            verb: "do",
+
+            answer: "did",
+
+            explanation:
+                "do → V2/ed: did.<br>" +
+                "did really well = đã làm rất tốt."
+        },
+
+        {
+            sentence:
+                "and my team (8) ________ the match 3-1!",
+
+            verb: "win",
+
+            answer: "won",
+
+            explanation:
+                "win → V2/ed: won.<br>" +
+                "won the match = đã thắng trận đấu."
+        },
+
+        {
+            sentence:
+                "It (9) ________ really cool!",
+
+            verb: "be",
+
+            answer: "was",
+
+            explanation:
+                "be → V2/ed: was.<br>" +
+                "was really cool = đã rất tuyệt."
+        },
+
+        {
+            sentence:
+                "After the match, my Dad took me to the cafe and (10) ________ me my favourite ice-cream!",
+
+            verb: "buy",
+
+            answer: "bought",
+
+            explanation:
+                "buy → V2/ed: bought.<br>" +
+                "bought me = đã mua cho tôi."
+        }
+
+    ],
+
+
+    /* =====================================================
+       TASK 6
+    ===================================================== */
+
+    task6: [
+
+        {
+            starter:
+                "A good club is...",
+
+            hint:
+                "Think about what makes a club interesting and enjoyable.",
+
+            sampleAnswer:
+                "A good club is fun, friendly, and interesting."
+        },
+
+        {
+            starter:
+                "People like it because...",
+
+            hint:
+                "Think about the activities, friends, teachers, or things students can learn.",
+
+            sampleAnswer:
+                "People like it because they can have fun and learn new things."
+        },
+
+        {
+            starter:
+                "In my school, there are...",
+
+            hint:
+                "Write about the clubs in your school.",
+
+            sampleAnswer:
+                "In my school, there are football, music, art, and computer clubs."
+        },
+
+        {
+            starter:
+                "Last year,...",
+
+            hint:
+                "Write about a club you joined or an activity you did last year.",
+
+            sampleAnswer:
+                "Last year, I joined the football club and played with my friends."
+        },
+
+        {
+            starter:
+                "In the future,...",
+
+            hint:
+                "Write about a club you want to join or start in the future.",
+
+            sampleAnswer:
+                "In the future, I want to join a music club and learn new songs."
+        }
+
+    ]
+
+};
