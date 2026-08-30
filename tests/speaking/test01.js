64d36818cf109561d1f5abb3dd7a3c784dca1e36
cophap <cobexuquang.abc@gmail.com>
2026-08-30 21:05:50 +0700

    Delete tests/speaking/test01.js

--- tests/speaking/test01.js
+++
@@ -1,9 +0,0 @@
-window.QUICKMARCH_TEST = {
-
-    title: "Speaking Test 01",
-
-    render: function(container) {
-        container.innerHTML = "<h2 style='text-align:center;color:#315b45;'>TEST 01 LOADED</h2>";
-    }
-
-};
