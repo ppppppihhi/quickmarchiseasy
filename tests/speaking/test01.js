9db3aa7c79b0c6785fda8ded8f15c020f97709b3
cophap <cobexuquang.abc@gmail.com>
2026-08-30 08:18:47 +0700

    Create test01.js

---
+++ tests/speaking/test01.js
@@ -0,0 +1,9 @@
+window.QUICKMARCH_TEST = {
+
+    title: "Speaking Test 01",
+
+    render: function(container) {
+        container.innerHTML = "<h2 style='text-align:center;color:#315b45;'>TEST 01 LOADED</h2>";
+    }
+
+};
